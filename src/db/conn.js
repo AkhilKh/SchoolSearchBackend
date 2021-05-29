9b5398fb73b65eae5e4abc95e772b90c8182f230
akhi945681 <akhi945681@gmail.com>
2021-05-29 20:34:37 +0530

    db string change

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://akhi945681:<Eduflow@765>@cluster0.qtm3a.mongodb.net/schoolsInfo?retryWrites=true&w=majority", {
+var MONGODB_URI = "mongodb+srv://akhi945681:Eduflow@765@cluster0.qtm3a.mongodb.net/schoolsInfo?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -8,6 +10,6 @@ mongoose.connect("mongodb+srv://akhi945681:<Eduflow@765>@cluster0.qtm3a.mongodb.
 }).then(() => {
     console.log("connection successful!");
 }).catch((e) => {
-    console.log("no connection");
+    console.log("no connection" + e);
 })
 
